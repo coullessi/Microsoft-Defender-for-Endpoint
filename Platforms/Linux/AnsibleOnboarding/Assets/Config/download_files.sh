39eae4d292bf5cf9fc9428fb82a48337d02be182
Lessi Coulibaly <coullessi@hotmail.com>
2024-06-01 10:02:58 -0700

    Initial commit

---
+++ Platforms/Linux/AnsibleOnboarding/Assets/Config/download_files.sh
@@ -0,0 +1,24 @@
+
+echo "Downloading the config_control_node.sh file"
+curl -o config_control_node.sh "https://raw.githubusercontent.com/coullessi/Microsoft-Defender-for-Endpoint/main/Platforms/Linux/AnsibleOnboarding/Assets/Config/config_controlnode.sh"
+
+echo "Downloading the hoss file, containing the IP addresses of the managed nodes"
+curl -o config_control_node.sh "https://raw.githubusercontent.com/coullessi/Microsoft-Defender-for-Endpoint/main/Platforms/Linux/AnsibleOnboarding/Assets/Config/hosts"
+
+echo "Downloading the onboard_setup.yml file"
+curl -o config_control_node.sh "https://raw.githubusercontent.com/coullessi/Microsoft-Defender-for-Endpoint/main/Platforms/Linux/AnsibleOnboarding/Assets/Config/onboard_setup.yml"
+
+echo "Downloading the dev_install_mdatp.yml file"
+curl -o config_control_node.sh "https://raw.githubusercontent.com/coullessi/Microsoft-Defender-for-Endpoint/main/Platforms/Linux/AnsibleOnboarding/Assets/Config/dev_install_mdatp.yml"
+
+echo "Downloading the dev_mdatp.repo.yml file"
+curl -o config_control_node.sh "https://raw.githubusercontent.com/coullessi/Microsoft-Defender-for-Endpoint/main/Platforms/Linux/AnsibleOnboarding/Assets/Config/dev_mdatp.repo.yml"
+
+echo "Downloading the prod_install_mdatp.yml file"
+curl -o config_control_node.sh "https://raw.githubusercontent.com/coullessi/Microsoft-Defender-for-Endpoint/main/Platforms/Linux/AnsibleOnboarding/Assets/Config/prod_install_mdatp.yml"
+
+echo "Downloading the prod_mdatp.repo.yml file"
+curl -o config_control_node.sh "https://raw.githubusercontent.com/coullessi/Microsoft-Defender-for-Endpoint/main/Platforms/Linux/AnsibleOnboarding/Assets/Config/prod_mdatp.repo.yml"
+
+echo "Downloading the unistall_mdatp.yml file"
+curl -o config_control_node.sh "https://raw.githubusercontent.com/coullessi/Microsoft-Defender-for-Endpoint/main/Platforms/Linux/AnsibleOnboarding/Assets/Config/unistall_mdatp.yml"
