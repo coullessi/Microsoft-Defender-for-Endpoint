99bc752714fa07e13e5f7c10696322aa08f73202
Lessi Coulibaly <coullessi@hotmail.com>
2024-06-01 12:03:03 -0700

    Initial commit

--- Platforms/Linux/ScriptOnboarding/install_mde.sh
+++ Platforms/Linux/ScriptOnboarding/install_mde.sh
@@ -2,4 +2,4 @@
 
 curl -o mde_installer.sh https://raw.githubusercontent.com/microsoft/mdatp-xplat/master/linux/installation/mde_installer.sh
 sudo unzip GatewayWindowsDefenderATPOnboardingPackage.zip
-sudo ./mde_installer.sh --install --channel prod --onboard MicrosoftDefenderATPOnboardingLinuxServer.py --tag GROUP "MDE-Management" --min_req -y
+sudo ./mde_installer.sh --install --channel prod --onboard MicrosoftDefenderATPOnboardingLinuxServer.py --tag GROUP "RedHat-Linux" --min_req -y
